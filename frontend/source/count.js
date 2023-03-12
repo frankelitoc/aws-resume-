0cfb2e2289f61b0eaa639ad73e7f42d81b828c63
frankie-11 <frank15ortiz@aol.com>
2023-03-12 01:44:55 -0500

    Counter JS

---
+++ frontend/source/count.js
@@ -0,0 +1,23 @@
+// The DOMContentLoaded event fires our visitcount function when the HTML document has been completely parsed
+window.addEventListener('DOMContentLoaded', (event) => {getVisitCount()});
+
+const apicall = 'https://1pohivmhq8.execute-api.us-east-1.amazonaws.com/default/LiveCounterFunction'
+
+
+const getVisitCount = () => {
+    let count;
+    fetch(functionApi)
+    .then(response => {
+        return response.json()
+    })
+    .then(response => {
+        console.log("API Function Called.")
+        console.log(response)
+        count = response;
+        //console.log(document.getElementById('counter').innerText);
+      //  document.getElementById('counter').innerText = count;
+    }).catch(function(error) {
+        console.log(error);
+      });
+    return count;
+}
